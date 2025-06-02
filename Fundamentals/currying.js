8f03c5546209e8aadbcce9b6045f96359ede4784
Vijay Mohan Vedhuruwada <avi369@vijays-mbp-2.lan>
2025-06-01 23:36:31 -0700

    add custom currying method

--- Fundamentals/currying.js
+++ Fundamentals/currying.js
@@ -4,3 +4,20 @@ const f = (a) => {
         return `${a + b}`
     }
 }
+
+// create currying method
+function curry(func) {
+    return function curried(...args) {
+        if (args.length >= func.length) {
+            return curry(...args);
+        } else {
+            return function(...next) {
+                return curried(...args, ...next);
+            }
+        }
+    }
+}
+
+const sum = (a, b, c, d) => a + b + c + d;
+const totalSum = curry(sum);
+console.log(totalSum(1)(3)(5)(7));
