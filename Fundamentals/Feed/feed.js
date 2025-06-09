e971e8bf75495af72c8b9fd56f1e41d9bc59903d
Vijay Mohan Vedhuruwada <avi369@vijays-mbp-2.lan>
2025-06-08 21:29:20 -0700

    Add theme toggle and dynamic form post

--- Fundamentals/Feed/feed.js
+++ Fundamentals/Feed/feed.js
@@ -16,14 +16,22 @@ const getPosts = async (offset, limit = 10) => {
 const renderPosts = (posts) => {
     const frag = document.createDocumentFragment();
     posts.forEach((post) => {
-        const postDiv = document.createElement("div");
-        postDiv.className = "post";
-        postDiv.innerHTML = `<h4>${post.title}</h4>
-            <p>${post.body}</p>    
-        `;
+        const postDiv = createPostCard(post, theme="light");
         frag.appendChild(postDiv);
     });
     feed.appendChild(frag);
+}
+
+// create Post card
+const createPostCard = (post) => {
+    const card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML = `
+        <h3>${post.title}</h3>
+        <p>${post.body}</p>
+        <small> Post ID: ${post.id} </small>
+    `;
+    return card;
 }
 
 // Load More Posts
@@ -44,9 +52,35 @@ const loadMorePosts = async () => {
 }
 
 window.addEventListener("scroll", () => {
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100) {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 300) {
         loadMorePosts();
     }
 });
 
 loadMorePosts();
+
+// Add a form to create and render new posts on top
+const form = document.getElementById("postForm");
+const postMap = new Map();
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const title = e.target.title.value.trim();
+    const body = e.target.body.value.trim();
+
+    if (!title || !body) return alert("all fields required");
+    const newPost = {
+        id: Date.now(),
+        title,
+        body
+    }
+    postMap.set(newPost.id, newPost);
+    feed.prepend(createPostCard(newPost));
+    form.reset();
+});
+
+const toggleBtn = document.getElementById("toggleTheme");
+toggleBtn.addEventListener('click', () => {
+    const root = document.documentElement;
+    const isDark = root.getAttribute("data-theme") === "dark";
+    root.setAttribute("data-theme", isDark ? "light" : "dark");
+});
